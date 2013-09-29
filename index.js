182c4129d655d1467dee6ec9d5c43c83504429a3
freewil <sean@eternalrise.com>
2013-09-28 20:51:53 -0700

    initial

---
+++ index.js
@@ -0,0 +1,15 @@
+var crypto = require('crypto');
+var scmp = require('scmp');
+
+module.exports = function dwollaWebhook(options) {
+  var secret = options.secret || '';
+  
+  return function(req, res, next) {
+    var sig = req.headers['x-dwolla-signature'] || '';
+    var hash = crypto.createHmac('sha1', secret).update(body).digest('hex');
+    
+    if (!scmp(sig, hash)) return res.send(403);
+    
+    next();
+  };
+};
